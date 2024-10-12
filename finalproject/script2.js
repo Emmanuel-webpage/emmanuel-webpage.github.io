b29c2f7bb6ad2e2099fb14e5c1482f919ff187d7
Emmanuel-webpage <3046821@gscs.ca>
2024-10-12 11:28:51 -0600

    Update script2.js

--- finalproject/script2.js
+++ finalproject/script2.js
@@ -1,5 +1,5 @@
 window.onload = (event) => {
-    alert("this is your quiz")
+    alert("this is your quiz"+"name")
 }
 document.getElementById('header').style.backgroundImage = "url('https://www.bing.com/th?id=OIP.hPQASS_f9GM9QaBdSAFvjwHaDo&w=231&h=104&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2')";
 const colors = ['red', 'green', 'blue', 'magenta', 'cyan'];
